4f6f1071bcbb0d8471c38872d875eab9814568b0
Douglas De Francesco <74323304+DouglasDeFrancesco@users.noreply.github.com>
2020-11-11 19:47:40 +0000

    Add files via upload

---
+++ Aula03.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	string nome;
+	cout << "Douglas \n";
+	cout << "Graca \n";
+	cin >> nome;
+	cout << "\n";
+	cout << nome;
+
+	// /n , pula linha
+	//cout << instrução de saida
+	// cin >> instruçaõ de entrada
+	// <iostream> bilblioteca para instruções de cout e cin
+
+
+	//aula 03
+
+	return 0;
+
+}
