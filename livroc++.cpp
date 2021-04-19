8454f47aef783e29474984f1d11c3df4c9556d49
Douglas De Francesco <74323304+DouglasDeFrancesco@users.noreply.github.com>
2021-04-19 22:48:56 +0100

    Add files via upload

---
+++ livroc++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+/* algoritimo em liguagem c++ */
+
+int main(int argc, char** argv) {
+
+	
+	int mdc  (int m, int) {
+		int r;
+		while ( ( r= m % n) ! = 0){
+			m=n;
+			n=r;
+		
+	}
+	return n;
+}
