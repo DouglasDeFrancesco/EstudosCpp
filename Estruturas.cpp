b11dd3e4024dd94df9bf598119f4f7817a9db270
Douglas De Francesco <74323304+DouglasDeFrancesco@users.noreply.github.com>
2021-05-10 20:38:45 +0100

    Add files via upload

--- Estruturas.cpp
+++ Estruturas.cpp
@@ -16,6 +16,7 @@ int main(int argc, char** argv) {
 	
 	// para fazer um conjunto de pessoas
 	pes turma[3]; // array de estruturas
+	pes aux;
 	
 	// escrever no campo "idade" da variável "pessoa"
 	pessoa.idade = 20; // dizer que a pessoa tem 20 anos
@@ -44,14 +45,67 @@ int main(int argc, char** argv) {
 	cout << "----------------------------" << endl;
 	// ler, usando um ciclo de repetição, os nomes e idades para
 	// todos os elementos da turma
-	(escreva aqui o seu código)
+	for(int i=0; i<3; i++){
+		cout << "Nome " << i+1 << ": ";
+		cin.clear(); cin.sync(); // com isto podem-se misturar cin com cin.getline()
+		cin.getline(turma[i].nome, 81);
+		cout << "Idade " << i+1 << ": ";
+		cin.clear(); cin.sync(); // com isto podem-se misturar cin com cin.getline()
+		cin >> turma[i].idade;
+	}
 	
 	
 	// Escrever, usando um ciclo de repetição, os nomes e idades da turma
-	(escreva aqui o seu código)
+	cout << "\nDados lidos das 3 pessoas\n"
+		 << "-------------------------\n";
+	for(int i=0; i<3; i++){
+		cout << "Nome " << i+1 << ": " << turma[i].nome << endl;
+		cout << "Idade " << i+1 << ": " << turma[i].idade << endl;
+	}
 	
 	// Pai de todos os desafios
 	// escrever as 3 pessoas ordenadas por idade
+	if(turma[0].idade>turma[1].idade){
+		// trocar as idades
+		aux.idade=turma[0].idade;
+		turma[0].idade=turma[1].idade;
+		turma[1].idade=aux.idade;
+		//trocar os nomes
+		strcpy(aux.nome, turma[0].nome);
+		strcpy(turma[0].nome, turma[1].nome);
+		strcpy(turma[1].nome, aux.nome);
+	}
+	// chegado aqui, n1 tem o menor dos 2
 	
+	if(turma[1].idade>turma[2].idade){
+		// trocar as idades
+		aux.idade=turma[1].idade;
+		turma[1].idade=turma[2].idade;
+		turma[2].idade=aux.idade;
+		//trocar os nomes
+		strcpy(aux.nome, turma[1].nome);
+		strcpy(turma[1].nome, turma[2].nome);
+		strcpy(turma[2].nome, aux.nome);
+	}
+	// chegado aqui, n3 tem o maior dos 3
+	
+	if(turma[0].idade>turma[1].idade){
+		// trocar as idades
+		aux.idade=turma[0].idade;
+		turma[0].idade=turma[1].idade;
+		turma[1].idade=aux.idade;
+		//trocar os nomes
+		strcpy(aux.nome, turma[0].nome);
+		strcpy(turma[0].nome, turma[1].nome);
+		strcpy(turma[1].nome, aux.nome);
+	}
+
+	// Escrever as pessoas ordenadas por idade
+	cout << "\nPessoas ordenadas por idade\n"
+		 << "---------------------------\n";
+	for(int i=0; i<3; i++){
+		cout << "Nome " << i+1 << ": " << turma[i].nome << endl;
+		cout << "Idade " << i+1 << ": " << turma[i].idade << endl;
+	}
 	return 0;
 }
